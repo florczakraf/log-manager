ba8be180ed32e63a9b8b5bfe548f83d6554b7f3c
Rafal Florczak <florczakraf@gmail.com>
2014-11-20 23:59:14 +0100

    [#1][#2] + moved db update to helper module

--- app/controllers/violations_controller.rb
+++ app/controllers/violations_controller.rb
@@ -2,24 +2,37 @@ class ViolationsController < ApplicationController
 
   def all
     show_per_page = 10
-    
-    @type_name = params[:type_name]
-    
-    if @type_name.nil? || @type_name == "ALL"
-      @violations = Violation.order(date: :desc).all.paginate(page: params[:page], per_page: show_per_page)
-    elsif @type_name != "ALL"
-      @violations = Violation.where("type_name LIKE '#{@type_name}'").order(date: :desc).paginate(page: params[:page], per_page: show_per_page)
-    end
-    
     @servers = Server.all
     @players = Player.all
     
-    @viol_types = [
-        ["ALL", "ALL"], ["MD5TOOL", "MD5TOOL"], ["CVAR", "CVAR"], ["GAMEHACK", "GAMEHACK"], ["DUPGUID", "DUPGUID"],
-        ["DUPNAME", "DUPNAME"], ["BADNAME", "BADNAME"], ["COMFAIL", "COMFAIL"], ["GAME INTEGRITY", "GAME INTEGRITY"], 
-        ["AUTO-UPDATE FAILURE", "AUTO-UPDATE FAILURE"], ["NAME SPAM", "NAME SPAM"]
-      ]
+    @viol_types = ["MD5TOOL", "CVAR", "GAMEHACK", "DUPGUID", "DUPNAME", "BADNAME",
+      "COMFAIL", "GAME INTEGRITY", "AUTO-UPDATE FAILURE", "NAME SPAM"]
     
+    violation_filter = params[:violation_filter]
+    server_filter = params[:server_filter]
+    
+    if violation_filter
+      session[:violation_filter] = violation_filter
+    end
+    
+    if server_filter
+      session[:server_filter] = server_filter
+    end
+    
+    @violation_filter = session[:violation_filter]
+    if @violation_filter.nil?
+      session[:violation_filter] = @viol_types
+      @violation_filter = @viol_types
+    end
+
+    @server_filter = session[:server_filter]
+    if @server_filter.nil?
+      @server_filter = []
+      @servers.each { |srv| @server_filter.push(srv.id) }
+      session[:server] = @server_filter
+    end
+    
+    @violations = Violation.where(type_name: @violation_filter, server_id: @server_filter).order(date: :desc).paginate(page: params[:page], per_page: show_per_page)
   end
   
   def single
--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,4 +1,65 @@
+require 'net/ftp'
+
 module PagesHelper
 
+  def updater
+    config = Setting.first
+    ip = config.ip
+    port = config.port
+    user = config.user
+    pass = config.pass
+    
+    connection = Net::FTP.new
+    connection.connect(ip, port)
+    connection.passive = true
+    connection.login(user, pass)
+    
+    servers = Server.all
+    
+    servers.each do |srv|
+      begin
+      connection.gettextfile("/109.169.20.171_#{srv.port}/pb/sv_viol.log", "tmp/#{srv.port}sv_viol.log")
+      
+      connection.gettextfile("/109.169.20.171_#{srv.port}/pb/sv_cheat.log", "tmp/#{srv.port}sv_cheat.log")
+      rescue
+      
+      end
+    end
+    
+    servers.each do |srv|
+      parse_into_db("tmp/#{srv.port}sv_viol.log", srv.id)
+      parse_into_db("tmp/#{srv.port}sv_cheat.log", srv.id)
+    end
+    connection.close
+  end
 
+  def parse_into_db(file, srv)
+    
+    pattern = %r{\[(\d\d.\d\d.\d\d\d\d) (\d\d:\d\d:\d\d)\] VIOLATION \(([A-Z\d\ -]+)\) #(\d+): (.+) \(slot \#\d+\) (.+) \[([a-f\d]+)\(.+\) ([\d\.]+)}
+    File.open(file).each  do |line|
+      if line =~ pattern
+        date = $1
+        time = $2
+        type = $3
+        type_id = $4
+        name = $5
+        details = $6
+        guid = $7
+        ip = $8
+        
+        datetime = date + ' ' + time
+        
+        player = Player.find_by_guid(guid)
+        if (player.nil?)
+          player = Player.create(:guid => guid, :banned => false)
+        end
+        
+        begin
+          viol = Violation.create(:date => datetime, :type_name => type, :player_id => player.id, :details => details, :ip => ip, :server_id => srv, :name => name)
+        rescue
+        
+        end
+      end
+    end
+  end
 end
